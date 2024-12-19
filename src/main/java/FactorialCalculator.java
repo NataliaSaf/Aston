31cfda404d9f139dac72b600815d51a34fd2aa54
NataliaSaf <natalasafina0060@gmail.com>
2024-12-19 13:11:42 +0500

    Задание выполнено
    Signed-off-by: NataliaSaf <natalasafina0060@gmail.com>

---
+++ src/main/java/FactorialCalculator.java
@@ -0,0 +1,10 @@
+public class FactorialCalculator {
+
+    public int factorial(int n) {
+        int result = 1;
+        for(int i =2; i <= n; i++){
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/FactorialCalculatorTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FactorialCalculatorTest {
+
+    private final FactorialCalculator calculator = new FactorialCalculator();
+
+    @Test
+    public void testFactorialOfThree(){
+        assertEquals(6, calculator.factorial(3));
+    }
+
+    @Test
+    public void testFactorialOfFour(){
+        assertEquals(20, calculator.factorial(4));
+    }
+
+    @Test
+    public void testFactorialOfFive(){
+        assertEquals(120, calculator.factorial(5));
+    }
+    @Test
+    public void testFactorialOSix(){
+        assertEquals(710, calculator.factorial(6));
+    }
+}
